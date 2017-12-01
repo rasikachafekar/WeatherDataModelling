b243557b253fe901c5192554e57f902b5437b5ed
rasikachafekar <rasika.chafekar@gmail.com>
2017-11-30 20:14:55 -0700

    NOAA pull by city with exception handling

--- WeatherPrediction/noaa.py
+++ WeatherPrediction/noaa.py
@@ -5,9 +5,23 @@ import datetime
 import json
 import pdb
 
+def get_csv_by_year(startyear,endyear):
+
+    for year in range(startyear, endyear) :
+        url = 'http://developer.nrel.gov/api/solar/nsrdb_0512_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes)
+        print(url)
+        try:
+            page = urlopen(url)
+        except urllib.error.HTTPError:
+            print("Exception occured ! Starting now from "+year)
+            get_csv_by_year(year, endyear)
+        req = Request(url,headers={'User-Agent': 'Mozilla/5.0'})
+        content = page.read().decode()
+        weatherCSV = open(str(year)+".csv","w")
+        weatherCSV.write(content)
 
 zipcode = 85281
-location="Tempe"
+location="Chicago"
 lat_long_api="https://maps.googleapis.com/maps/api/geocode/json?address="+location
 #lat_long_api = "http://maps.googleapis.com/maps/api/geocode/json?address="+str(zipcode)
 page = urlopen(lat_long_api)
@@ -37,7 +51,7 @@ your_email = 'js@sas.edu'
 # Please join our mailing list so we can keep you up-to-date on new developments.
 mailing_list = 'true'
 y = datetime.date.today().year
-year=y-2
+year=y-1
 
 
 # url = 'http://developer.nrel.gov/api/solar/nsrdb_0512_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes)
@@ -48,13 +62,9 @@ year=y-2
 # #print(content)
 # weatherCSV = open(year+".csv","w")
 # weatherCSV.write(content)
+get_csv_by_year(year-15, year)
 
-for year in range(year, year-15 ,-1) :
-    url = 'http://developer.nrel.gov/api/solar/nsrdb_0512_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes)
-    print(url)
-    page = urlopen(url)
-    req = Request(url,headers={'User-Agent': 'Mozilla/5.0'})
-    content = page.read().decode()
-    weatherCSV = open(str(year)+".csv","w")
-    weatherCSV.write(content)
+
+
+
 
