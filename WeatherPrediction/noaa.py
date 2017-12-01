4881df2c9d62f90387756be3bd030c89ec5003fe
rasikachafekar <rasika.chafekar@gmail.com>
2017-11-30 19:53:24 -0700

    NOAA api call loop good

--- WeatherPrediction/noaa.py
+++ WeatherPrediction/noaa.py
@@ -7,7 +7,9 @@ import pdb
 
 
 zipcode = 85281
-lat_long_api = "http://maps.googleapis.com/maps/api/geocode/json?address="+str(zipcode)
+location="Tempe"
+lat_long_api="https://maps.googleapis.com/maps/api/geocode/json?address="+location
+#lat_long_api = "http://maps.googleapis.com/maps/api/geocode/json?address="+str(zipcode)
 page = urlopen(lat_long_api)
 req = Request(lat_long_api, headers={'User-Agent': 'Mozilla/5.0'})
 content = json.loads(page.read().decode())
@@ -17,7 +19,7 @@ lat = lat_long["lat"]
 lon = lat_long["lng"]
 
 
-api_key = '3KEle87nPOrTBNXBVWeXqChGztQm0pvTLbepwVwW'
+api_key = 'bc9ezc45D4YpJhKyZ5ywjprQCTDFVtkx6JjYUNE1'
 
 attributes = 'ghi,wind_speed_10m_nwp,surface_air_temperature_nwp'
 leap_year = 'false'
@@ -25,19 +27,21 @@ leap_year = 'false'
 interval = '30'
 # Specify Coordinated Universal Time (UTC), 'true' will use UTC, 'false' will use the local time zone of the data.
 utc = 'false'
-your_name = 'John+Smith'
+your_name = 'Rasika'
 # Your reason for using the NSRDB.
 reason_for_use = 'beta+testing'
 # Your affiliation
-your_affiliation = 'my+institution'
+your_affiliation = 'ASU'
 # Your email address
-your_email = 'john.smith@server.com'
+your_email = 'js@sas.edu'
 # Please join our mailing list so we can keep you up-to-date on new developments.
 mailing_list = 'true'
-year = datetime.date.today().year
+y = datetime.date.today().year
+year=y-2
 
 
 # url = 'http://developer.nrel.gov/api/solar/nsrdb_0512_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes)
+# print (url)
 # page = urlopen(url)
 # req = Request(url,headers={'User-Agent': 'Mozilla/5.0'})
 # content = page.read().decode()
@@ -46,10 +50,11 @@ year = datetime.date.today().year
 # weatherCSV.write(content)
 
 for year in range(year, year-15 ,-1) :
-    url = 'http://developer.nrel.gov/api/solar/nsrdb_0512_download.csv?wkt=POINT({lon}%20{lat})&names={year}&interval={interval}&utc=false&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, interval=interval, utc=utc, api=api_key,attr=attributes)
+    url = 'http://developer.nrel.gov/api/solar/nsrdb_0512_download.csv?wkt=POINT({lon}%20{lat})&names={year}&leap_day={leap}&interval={interval}&utc={utc}&full_name={name}&email={email}&affiliation={affiliation}&mailing_list={mailing_list}&reason={reason}&api_key={api}&attributes={attr}'.format(year=year, lat=lat, lon=lon, leap=leap_year, interval=interval, utc=utc, name=your_name, email=your_email, mailing_list=mailing_list, affiliation=your_affiliation, reason=reason_for_use, api=api_key, attr=attributes)
+    print(url)
     page = urlopen(url)
     req = Request(url,headers={'User-Agent': 'Mozilla/5.0'})
     content = page.read().decode()
-    weatherCSV = open(year+".csv","w")
+    weatherCSV = open(str(year)+".csv","w")
     weatherCSV.write(content)
 
